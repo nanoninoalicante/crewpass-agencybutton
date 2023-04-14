f2cc5e9f6e95c7796380b8984b2fc02fd585d1cb
Christopher Hill <cjameshill@gmail.com>
2023-04-14 14:10:57 +0200

    Added commit id into build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 const ENVIRONMENT = process.env.ENVIRONMENT;
+const COMMIT_ID = process.env.COMMIT_ID || "commitId";
 
 module.exports = (env) => {
   return {
@@ -30,8 +31,9 @@ module.exports = (env) => {
         "process.env.CLIENT_DATA_URL": JSON.stringify(
           process.env.CLIENT_DATA_URL
         ),
+        "process.env.ENVIRONMENT": JSON.stringify(ENVIRONMENT || ""),
         "process.env.VERSION": JSON.stringify(process.env.VERSION || ""),
-        "process.env.COMMIT_ID": JSON.stringify(process.env.COMMIT_ID || ""),
+        "process.env.COMMIT_ID": JSON.stringify(COMMIT_ID),
         "process.env.POPUP_URL": JSON.stringify(process.env.POPUP_URL || ""),
         "process.env.BASE_CDN_URL": JSON.stringify(process.env.BASE_CDN_URL || ""),
       }),
